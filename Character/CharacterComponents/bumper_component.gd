b887eebbf8ce6f5b9c881724b4b18e10827a6a3e
Mikołaj Wirkijowski <lupsik@wp.pl>
2025-01-30 14:41:00 +0100

    Generate proper navmesh for chocolate factory

--- Character/CharacterComponents/bumper_component.gd
+++ Character/CharacterComponents/bumper_component.gd
@@ -4,6 +4,9 @@ class_name BumperComponent
 @export var character: NonPlayerCharacter
 @export var sliding_speed: float
 
+func _ready():
+		
+	
 func _process(delta):
 	if is_colliding():
 		character.velocity += get_collision_normal() * sliding_speed * delta
