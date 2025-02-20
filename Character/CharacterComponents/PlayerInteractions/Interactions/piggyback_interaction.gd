684c3582707921341b14107b66448b6cf8495db0
Mikołaj Wirkijowski <lupsik@wp.pl>
2025-02-20 18:00:28 +0100

    Fix bad function call, finish up before leaving for vacation

--- Character/CharacterComponents/PlayerInteractions/Interactions/piggyback_interaction.gd
+++ Character/CharacterComponents/PlayerInteractions/Interactions/piggyback_interaction.gd
@@ -7,7 +7,7 @@ func _condition_filled() -> bool:
 	.distance_to(PlayerDeviceManager.get_other_player(initiator).global_position)
 
 	return distance < MAX_DISTANCE and \
-	sightline_clear_between_players(initiator,receiver)
+	sightline_clear_between_players()
 
 func _action() -> void:
-	
+	pass
